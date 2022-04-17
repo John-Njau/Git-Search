38af4bdcf80d8395ff372a74d111b4571125e007
john.njau <john.njoroge@student.moringaschool.com>
2022-04-17 15:20:52 +0300

    ft: home component

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service'
+import { UserInterface } from '../../class/user'
+import { Userprofile } from '../../userprofile'
+import { FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  username: string; //username
+  gitprofile!: Userprofile; //profile
+
+  constructor(private userService: UserService) {
+    // sort of placeholder at the input section
+    this.username = `${this.userService.searchuser}`
+    console.log(this.username);
+
+  }
+
+  searchGithub() {
+    this.userService.updateName(this.username);
+    this.userService.githubUser().subscribe(
+      user => { this.gitprofile = user }
+    )
+    // this.gitprofile = <any>this.userService.searchuser
+    // alert(this.gitprofile);
+
+  }
 
   ngOnInit(): void {
+    this.userService.githubUser().subscribe(
+      data => {
+        this.gitprofile = data
+        console.log(this.gitprofile);
+
+      }
+    )
   }
 
 }
