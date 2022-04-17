0c4ccf192be2b9418eab7dd534110e0ff6f38398
john.njau <john.njoroge@student.moringaschool.com>
2022-04-17 15:18:05 +0300

    ft: created a user profile class

---
+++ src/app/userprofile.ts
@@ -0,0 +1,22 @@
+export class Userprofile {
+    constructor(
+        public id: number,
+        public followers: number,
+        public following: number,
+        public url: string,
+        public name: string,
+        public blog: string,
+        public repos: string,
+        public company: string,
+        public login: string,
+        public html_url: any,
+        public updated_at: string,
+        public created_at: string,
+        public avatar_url: string,
+        public followers_url: string,
+        public public_repos: string,
+        public repos_url: string,
+    ) {
+
+    }
+}
