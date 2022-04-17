a6ce3e1c2b3c0e176651dc1e22a5fb672a4c2db3
john.njau <john.njoroge@student.moringaschool.com>
2022-04-17 23:16:48 +0300

    ft: repo & user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,10 +11,13 @@ import { RepoInterface } from '../class/repo';
 export class UserService {
   search!: Userprofile
   searchuser = `john-njau`
+
+  ExistingRepos: any;
+  searchRepo = "gitsearch" //repo search
   // private Git_token= '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';
 
   GITHUB_API_URL = `https://api.github.com/users/`
-  REPO_URL = `https://api.github.com/users/`
+  REPO_URL = `https://api.github.com/search/repositories?q=`
 
   constructor(private http: HttpClient) {
     // this.search = new Userprofile(0,0,0,'','','','','','','','','','','','','')
@@ -27,11 +30,19 @@ export class UserService {
     // this.search.followers;
   }
 
-  githubRepos(): Observable<RepoInterface> {
-    return this.http.get<RepoInterface>(this.REPO_URL + this.searchuser + `/repos`);
+  getUserRepos(): Observable<RepoInterface> {
+    return this.http.get<RepoInterface>(this.GITHUB_API_URL + this.searchuser + "/repos");
+  }
+
+  searchGithubRepo(): Observable<RepoInterface> {
+    return this.http.get<RepoInterface>(this.REPO_URL + this.searchRepo + "");
   }
 
   updateName(userName: string) {
     this.searchuser = userName;
-    }
+  }
+
+  updateRepo(repoName: string) {
+    this.searchRepo = repoName;
+  }
 }
