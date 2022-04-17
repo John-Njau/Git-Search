bd4fd6caf1c53b9155de75cbeecf648bb0722868
john.njau <john.njoroge@student.moringaschool.com>
2022-04-17 15:17:22 +0300

    ft: Import modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppComponent } from './app.component';
 import { GitUserDirective } from './directives/git-user.directive';
 import { FilterSearchPipe } from './pipes/filter-search.pipe';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,13 @@ import { HomePageComponent } from './components/home-page/home-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    Ng2SearchPipeModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
