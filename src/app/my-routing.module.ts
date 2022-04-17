5663060edd17f0ae9e69c5d3844ccfcea9289ac3
john.njau <john.njoroge@student.moringaschool.com>
2022-04-17 23:09:47 +0300

    ft: custom routing module implementation

--- src/app/my-routing.module.ts
+++ src/app/my-routing.module.ts
@@ -1,1 +1,18 @@
-import {}
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { AboutComponent } from "./components/about/about.component";
+import { RepoComponent } from "./components/repo/repo.component";
+
+const routes: Routes = [
+    { path: "home", component: HomePageComponent },
+    { path: "about", component: AboutComponent },
+    { path: "repositories", component: RepoComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class MyRoutingModule { }
